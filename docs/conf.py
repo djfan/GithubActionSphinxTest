ebab1245f1d874c1d1ade5487cb065f69f201e06
Dongjie Fan <Dongjie.Fan@footlocker.com>
2021-08-13 18:16:30 -0400

    new 4

--- docs/conf.py
+++ docs/conf.py
@@ -15,6 +15,8 @@ import sys
 sys.path.insert(0, os.path.abspath('.'))
 sys.path.insert(0, os.path.abspath('../'))
 sys.path.insert(0, os.path.abspath('examplecode/'))
+sys.path.insert(0, os.path.abspath('examplecode'))
+sys.path.insert(0, os.path.relpath('../examplecode'))
 
 
 # -- Project information -----------------------------------------------------
