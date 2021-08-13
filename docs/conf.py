4bafacc9acce6af6d4bca54246634fc75bdc28da
Dongjie Fan <Dongjie.Fan@footlocker.com>
2021-08-13 18:03:46 -0400

    new 4

--- docs/conf.py
+++ docs/conf.py
@@ -10,16 +10,17 @@
 # add these directories to sys.path here. If the directory is relative to the
 # documentation root, use os.path.abspath to make it absolute, like shown here.
 #
-# import os
-# import sys
-# sys.path.insert(0, os.path.abspath('.'))
+import os
+import sys
+sys.path.insert(0, os.path.abspath('.'))
 
 
 # -- Project information -----------------------------------------------------
 
-project = 'No Errors Test Project'
-copyright = '2019, sphinx-action Test Suite'
-author = 'sphinx-action Test Suite'
+# General information about the project.
+project = u'examplecode'
+copyright = u'2017, Tim'
+author = u'Tim'
 
 
 # -- General configuration ---------------------------------------------------
@@ -50,5 +51,7 @@ html_theme = 'alabaster'
 # relative to this directory. They are copied after the builtin static files,
 # so a file named "default.css" will overwrite the builtin "default.css".
 html_static_path = ['_static']
+
+htmlhelp_basename = 'examplecodedoc'
 
 master_doc = 'index'
