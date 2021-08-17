b6ace2e6b7153044b7381ab215772ddd1ab68c97
Dongjie Fan <Dongjie.Fan@footlocker.com>
2021-08-17 15:01:16 -0400

    add class example

--- examplecode/google_style_example.py
+++ examplecode/google_style_example.py
@@ -26,7 +26,6 @@ Attributes:
 Todo:
     * For module TODOs
     * You have to also use ``sphinx.ext.todo`` extension
-
 .. _Google Python Style Guide:
    https://google.github.io/styleguide/pyguide.html
 
