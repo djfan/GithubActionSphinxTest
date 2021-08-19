5bb3a4d75f28cd935880e3defd1c3cc4ac59238b
Dongjie Fan <Dongjie.Fan@footlocker.com>
2021-08-19 14:40:54 -0400

    fix

---
+++ examplecode/store.py
@@ -0,0 +1,11 @@
+def get_latlng():
+    """
+    A function to return the square of X. hahahah
+
+    Args:
+        x (float): A float or numpy array
+
+    Returns:
+        float: The value of x-squared
+    """
+    return None
