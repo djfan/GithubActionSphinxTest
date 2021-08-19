2e4b7affdee1edbbf1fe34946055950da552e0d7
Dongjie Fan <Dongjie.Fan@footlocker.com>
2021-08-19 17:25:47 -0400

    new layout4

--- examplecode/__init__.py
+++
@@ -1,6 +0,0 @@
-# from .retail.store import get_latlng2
-
-# __all__ = [
-#     '__version__',
-#     'get_latlng2',
-# ]
--- examplecode/retail/__init__.py
+++ examplecode/retail/__init__.py
@@ -1,7 +1,7 @@
-from .store import get_latlng
+# from .store import get_latlng
 from .store import get_latlng2
 
 __all__ = [
-    get_latlng,
+    #get_latlng,
     get_latlng2
 ]
